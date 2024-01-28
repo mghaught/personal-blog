d01bab7b1b2c436112b87eb7e69e3512021ae7d8
Scriptifex <natalie.haught@gmail.com>
2024-01-27 19:47:29 -0700

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+      '_includes/**/*.html',
+      '_layouts/**/*.html',
+      '*.markdown'
+  ],
+  theme: {
+      extend: {},
+  },
+  daisyui: {
+    themes: [
+      //"emerald",
+      //"autumn",
+      "winter",
+      "bumblebee",
+      "lemonade",
+      "forest",
+      "sunset",
+      "bumblebee",
+      "autumn",
+      "emerald",
+      {
+        mytheme: {
+          primary: "#0099ff",
+          secondary: "#66ccff",
+          accent: "#99ffcc",
+          neutral: "#cccccc",
+          "base-100": "#ffffff",
+          info: "#00cc99",
+          success: "#33cccc",
+          warning: "#ffffff",
+          error: "#ffffff",
+        },
+      },
+    ],
+    darkTheme: "sunset",
+    base: true,
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
